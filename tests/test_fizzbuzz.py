9a606aa5f352574ff12cc12dd50cba06b90cadef
ijknabla <ijknabla@gmail.com>
2023-05-29 21:58:29 +0900

    Implement test_fizzbuzz

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,5 +1,15 @@
-from grpc import Server
+import re
+
+from mine.rpc import MineStub
+from mine.types import FizzBuzzRequest
 
 
-def test_fizzbuzz(grpc_server: Server) -> None:
-    ...
+def test_fizzbuzz(grpc_stub: MineStub) -> None:
+    for i in range(100):
+        request = FizzBuzzRequest(i=i)
+        response = grpc_stub.FizzBuzz(request)
+        matched = re.match(r"^(Fizz)?(Buzz)?$", response.s)
+        assert matched is not None
+        fizz, buzz = matched.groups()
+        assert (fizz is not None) == (i % 3 == 0)
+        assert (buzz is not None) == (i % 5 == 0)
