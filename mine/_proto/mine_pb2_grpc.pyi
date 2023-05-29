41e94719087cdd716b0220e1870df284a977d299
ijknabla <ijknabla@gmail.com>
2023-05-29 22:10:24 +0900

    Add type-hint for Count rpc

--- mine/_proto/mine_pb2_grpc.pyi
+++ mine/_proto/mine_pb2_grpc.pyi
@@ -1,14 +1,23 @@
+from collections.abc import Iterator as _Iterator
+
 import grpc
+from grpc._channel import _MultiThreadedRendezvous
 
 from . import mine_pb2 as mine__pb2
 
 class MineStub(object):
     def __init__(self, channel: grpc.Channel) -> None: ...
     def FizzBuzz(self, request: mine__pb2.FizzBuzzRequest) -> mine__pb2.FizzBuzzResponse: ...
+    def Count(
+        self, request: mine__pb2.CountRequest
+    ) -> _MultiThreadedRendezvous[mine__pb2.CountResponse]: ...
 
 class MineServicer(object):
     def FizzBuzz(
         self, request: mine__pb2.FizzBuzzRequest, context: grpc.ServicerContext
     ) -> mine__pb2.FizzBuzzResponse: ...
+    def Count(
+        self, request: mine__pb2.CountRequest, context: grpc.ServicerContext
+    ) -> _Iterator[mine__pb2.CountResponse]: ...
 
 def add_MineServicer_to_server(servicer: MineServicer, server: grpc.Server) -> None: ...
