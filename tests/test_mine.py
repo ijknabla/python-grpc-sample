faaac3d69bc0682d2e547944d025385075319ed5
ijknabla <ijknabla@gmail.com>
2023-05-30 12:33:51 +0900

    Update fizzbuzz test

--- tests/test_mine.py
+++ tests/test_mine.py
@@ -1,17 +1,35 @@
 import re
+from collections.abc import Awaitable
 
-from mine.pb2 import CountRequest, FizzBuzzRequest, MineStub
+import pytest
+
+from mine.pb2 import AsyncMineStub, CountRequest, FizzBuzzRequest, FizzBuzzResponse, MineStub
+
+AnyMineStub = MineStub | AsyncMineStub
 
 
-def test_fizzbuzz(grpc_stub: MineStub) -> None:
+@pytest.mark.asyncio
+async def test_fizzbuzz(grpc_stub: MineStub) -> None:
+    await _test_any_fizzbuzz(grpc_stub)
+
+
+async def _test_any_fizzbuzz(stub: AnyMineStub) -> None:
     for i in range(100):
         request = FizzBuzzRequest(i=i)
-        response = grpc_stub.FizzBuzz(request)
+        response = await _call_fizzbuzz(stub, request)
         matched = re.match(r"^(Fizz)?(Buzz)?$", response.s)
         assert matched is not None
         fizz, buzz = matched.groups()
         assert (fizz is not None) == (i % 3 == 0)
         assert (buzz is not None) == (i % 5 == 0)
+
+
+async def _call_fizzbuzz(stub: AnyMineStub, request: FizzBuzzRequest) -> FizzBuzzResponse:
+    response = stub.FizzBuzz(request)
+    if isinstance(response, Awaitable):
+        return await response
+    else:
+        return response
 
 
 def test_count(grpc_stub: MineStub) -> None:
