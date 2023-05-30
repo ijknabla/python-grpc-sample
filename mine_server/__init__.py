a43286bca2bcd496bff70a0efb7ba05f2a3debfe
ijknabla <ijknabla@gmail.com>
2023-05-30 11:31:58 +0900

    Fix import

--- mine_server/__init__.py
+++ mine_server/__init__.py
@@ -2,8 +2,7 @@ from collections.abc import Generator, Iterator
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 
-from grpc import ServicerContext
-from grpc import server as grpc_server
+import grpc
 
 from mine import CountRequest, CountResponse, FizzBuzzRequest, FizzBuzzResponse
 from mine import MineServicer as MineServicerBase
@@ -11,7 +10,7 @@ from mine import add_MineServicer_to_server
 
 
 class MineServicer(MineServicerBase):
-    def FizzBuzz(self, request: FizzBuzzRequest, context: ServicerContext) -> FizzBuzzResponse:
+    def FizzBuzz(self, request: FizzBuzzRequest, context: grpc.ServicerContext) -> FizzBuzzResponse:
         response = FizzBuzzResponse()
         if request.i % 3 == 0:
             response.s += "Fizz"
@@ -19,7 +18,9 @@ class MineServicer(MineServicerBase):
             response.s += "Buzz"
         return response
 
-    def Count(self, request: CountRequest, context: ServicerContext) -> Iterator[CountResponse]:
+    def Count(
+        self, request: CountRequest, context: grpc.ServicerContext
+    ) -> Iterator[CountResponse]:
         for i in range(request.n):
             yield CountResponse(i=i)
 
@@ -27,7 +28,7 @@ class MineServicer(MineServicerBase):
 @contextmanager
 def serve(host: str, port: int) -> Generator[None, None, None]:
     servicer = MineServicer()
-    server = grpc_server(ThreadPoolExecutor(max_workers=3))
+    server = grpc.server(ThreadPoolExecutor(max_workers=3))
     add_MineServicer_to_server(servicer, server)
     server.add_insecure_port(f"{host}:{port}")
     server.start()
