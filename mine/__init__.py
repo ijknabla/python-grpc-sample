d442e223a18d90c7c9602d444f7b6f9051e71100
ijknabla <ijknabla@gmail.com>
2023-05-30 09:22:46 +0900

    Fix Stub definition

--- mine/__init__.py
+++ mine/__init__.py
@@ -8,5 +8,36 @@ __all__ = (
     "MineStub",
     "add_MineServicer_to_server",
 )
+from collections.abc import AsyncIterable, Awaitable, Iterator
+from typing import TYPE_CHECKING
+
+import grpc.aio
+
 from ._proto.mine_pb2 import CountRequest, CountResponse, FizzBuzzRequest, FizzBuzzResponse
-from ._proto.mine_pb2_grpc import AsyncMineStub, MineServicer, MineStub, add_MineServicer_to_server
+from ._proto.mine_pb2_grpc import MineServicer, add_MineServicer_to_server
+
+if TYPE_CHECKING:
+
+    class MineStub(object):
+        def __init__(self, channel: grpc.Channel) -> None:
+            ...
+
+        def FizzBuzz(self, request: FizzBuzzRequest) -> FizzBuzzResponse:
+            ...
+
+        def Count(self, request: CountRequest) -> Iterator[CountResponse]:
+            ...
+
+    class AsyncMineStub(object):
+        def __init__(self, channel: grpc.aio.Channel) -> None:
+            ...
+
+        def FizzBuzz(self, request: FizzBuzzRequest) -> Awaitable[FizzBuzzResponse]:
+            ...
+
+        def Count(self, request: CountRequest) -> AsyncIterable[CountResponse]:
+            ...
+
+else:
+    from ._proto.mine_pb2_grpc import MineStub as AsyncMineStub
+    from ._proto.mine_pb2_grpc import MineStub as MineStub
