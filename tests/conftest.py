50b2a9342a8ec3a62a727a0bd2dd3d56b2fffc7b
ijknabla <ijknabla@gmail.com>
2023-05-30 10:01:39 +0900

    Implement MineStubWrapper

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from asyncio import AbstractEventLoop, get_event_loop_policy
-from collections.abc import AsyncGenerator, Generator
-from typing import Protocol
+from asyncio import AbstractEventLoop, get_event_loop_policy, get_running_loop
+from collections.abc import AsyncGenerator, AsyncIterable, Awaitable, Callable, Generator
+from dataclasses import dataclass
+from typing import Protocol, TypeVar
 
 import grpc.aio
 from pytest import FixtureRequest
@@ -10,11 +11,34 @@ from pytest_asyncio import fixture
 
 from mine import (
     AsyncMineStub,
+    CountRequest,
+    CountResponse,
+    FizzBuzzRequest,
+    FizzBuzzResponse,
     MineStub,
     SupportsAddMineServicerToServer,
     add_MineServicer_to_server,
 )
 from mine_server import MineServicer
+
+T = TypeVar("T")
+
+
+@dataclass(frozen=True)
+class MineStubWrapper:
+    stub: MineStub
+
+    @staticmethod
+    def run(f: Callable[[], T]) -> Awaitable[T]:
+        return get_running_loop().run_in_executor(None, f)
+
+    def FizzBuzz(self, request: FizzBuzzRequest) -> Awaitable[FizzBuzzResponse]:
+        return self.run(lambda: self.stub.FizzBuzz(request))
+
+    async def Count(self, request: CountRequest) -> AsyncIterable[CountResponse]:
+        responses = await self.run(lambda: list(self.stub.Count(request)))
+        for response in responses:
+            yield response
 
 
 @fixture(scope="session")
