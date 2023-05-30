5c33455e6e9e30dd6e00ff107fe83a0ea7199140
ijknabla <ijknabla@gmail.com>
2023-05-30 18:16:17 +0900

    Move {mine.run => mine_server.__main__.run_main}

--- mine/__init__.py
+++ mine/__init__.py
@@ -1,22 +1,8 @@
-import asyncio
 import sys
-from collections.abc import Callable, Coroutine, Generator
+from collections.abc import Generator
 from contextlib import contextmanager
-from functools import wraps
-from typing import Any, ParamSpec, TypeVar
 
 from pkg_resources import resource_filename
-
-_P = ParamSpec("_P")
-_T = TypeVar("_T")
-
-
-def run(f: Callable[_P, Coroutine[Any, Any, _T]]) -> Callable[_P, _T]:
-    @wraps(f)
-    def wrapped(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-        return asyncio.run(f(*args, **kwargs))
-
-    return wrapped
 
 
 @contextmanager
--- mine_server/__main__.py
+++ mine_server/__main__.py
@@ -1,16 +1,28 @@
-from asyncio import sleep
+from asyncio import run, sleep
+from collections.abc import Callable, Coroutine
+from functools import wraps
+from typing import Any, ParamSpec, TypeVar
 
 import click
 
-from mine import run
-
 from . import serve
+
+_P = ParamSpec("_P")
+_T = TypeVar("_T")
+
+
+def run_main(f: Callable[_P, Coroutine[Any, Any, _T]]) -> Callable[_P, _T]:
+    @wraps(f)
+    def wrapped(*args: _P.args, **kwargs: _P.kwargs) -> _T:
+        return run(f(*args, **kwargs))
+
+    return wrapped
 
 
 @click.command()
 @click.option("--host", type=str, default="127.0.0.1")
 @click.option("--port", type=int, required=True)
-@run
+@run_main
 async def main(
     host: str,
     port: int,
