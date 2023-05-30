ec3d98a34a88e1cb121f51cf50f74a1acc1b9831
ijknabla <ijknabla@gmail.com>
2023-05-30 11:45:03 +0900

    Implement AsyncMineServicer

--- mine_server/__init__.py
+++ mine_server/__init__.py
@@ -1,15 +1,16 @@
-from collections.abc import Generator, Iterator
+from collections.abc import AsyncIterator, Generator, Iterator
 from concurrent.futures import ThreadPoolExecutor
 from contextlib import contextmanager
 
 import grpc
 
+from mine import AsyncMineServicer as _AsyncMineServicerBase
 from mine import CountRequest, CountResponse, FizzBuzzRequest, FizzBuzzResponse
-from mine import MineServicer as MineServicerBase
+from mine import MineServicer as _MineServicerBase
 from mine import add_MineServicer_to_server
 
 
-class MineServicer(MineServicerBase):
+class MineServicer(_MineServicerBase):
     def FizzBuzz(self, request: FizzBuzzRequest, context: grpc.ServicerContext) -> FizzBuzzResponse:
         response = FizzBuzzResponse()
         if request.i % 3 == 0:
@@ -21,6 +22,26 @@ class MineServicer(MineServicerBase):
     def Count(
         self, request: CountRequest, context: grpc.ServicerContext
     ) -> Iterator[CountResponse]:
+        for i in range(request.n):
+            yield CountResponse(i=i)
+
+
+class AsyncMineServicer(_AsyncMineServicerBase):
+    async def FizzBuzz(
+        self,
+        request: FizzBuzzRequest,
+        context: grpc.aio.ServicerContext[FizzBuzzRequest, FizzBuzzResponse],
+    ) -> FizzBuzzResponse:
+        response = FizzBuzzResponse()
+        if request.i % 3 == 0:
+            response.s += "Fizz"
+        if request.i % 5 == 0:
+            response.s += "Buzz"
+        return response
+
+    async def Count(
+        self, request: CountRequest, context: grpc.aio.ServicerContext[CountRequest, CountResponse]
+    ) -> AsyncIterator[CountResponse]:
         for i in range(request.n):
             yield CountResponse(i=i)
 
