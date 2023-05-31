337ee1e24530979063b623d37320e974b5bb155e
ijknabla <ijknabla@gmail.com>
2023-05-31 14:16:37 +0900

    Implement _message module

---
+++ mine/_message.py
@@ -0,0 +1,10 @@
+from ._proto import mine_pb2 as _
+
+FizzBuzzRequest = _.FizzBuzzRequest
+FizzBuzzResponse = _.FizzBuzzResponse
+
+CountRequest = _.UnsignedInteger
+CountResponse = _.UnsignedInteger
+
+SumRequest = _.UnsignedInteger
+SumResponse = _.UnsignedInteger
